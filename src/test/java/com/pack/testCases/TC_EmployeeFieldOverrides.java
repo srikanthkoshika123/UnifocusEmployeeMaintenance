ab30aea0d65d54b8c2e7f7007b01d4388485ca2f
srikanthkoshika123 <srikanth@ecosleek.in>
2021-12-06 19:43:28 +0530

    labor updates

--- src/test/java/com/pack/testCases/TC_EmployeeFieldOverrides.java
+++ src/test/java/com/pack/testCases/TC_EmployeeFieldOverrides.java
@@ -14,20 +14,7 @@ public class TC_EmployeeFieldOverrides extends TC_BaseClass{
 	 @Test
 		public void employeeFieldOverrides() throws  Exception {
 	    ScreenRecorderUtil.startRecord("EmployeeFieldOverrides");
-	    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    	LoginPage lp=new LoginPage(driver);
-	    	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	    	lp.setPartnerCode(partnerCode);
-	    	lp.clickNext();
-		    logger.info("enter user name");
-		    driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-	     	lp.setUserName(loginas);
-	     	logger.info("password entered");
-	        lp.setPassWord(Password);
-			Thread.sleep(4000);
-	    	lp.clickSignIn();
-	    	logger.info("signin clicked");
-	    	Thread.sleep(6000);
+	    	
 	    	EmployeeMaintenance employeemaintenance=new EmployeeMaintenance(driver);
 			employeemaintenance.clickUnifocus();
 			EmployeeFieldOverrides efo=new EmployeeFieldOverrides(driver);
